621b5d2438b513894bb4ebb30ca159691ee0efde
Syed-Masrur-Ahmed <98674956+Syed-Masrur-Ahmed@users.noreply.github.com>
2022-03-15 10:35:06 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -2,9 +2,7 @@ from flask import Flask, render_template, request, redirect
 import pandas as pd
 from io import StringIO
 import matplotlib.pyplot as plt
-from time import sleep
 import os
-import asyncio
 
 plt.style.use('fivethirtyeight')
 
@@ -12,170 +10,143 @@ my_path = "/".join(os.path.abspath(__file__).split("\\")[0:-1]) + "/static/"
 
 app = Flask(__name__)
 
-global columns
 columns = []
-col_rep = -1
 
 data_ = []
-data_rep = -1
-
 data_string = ""
+
 graph = []
-graph_rep = -1
+# l = Line Graph; b = Bar Graph; s = Scatter Plot
+l = False
+b = False
+s = False
 
-keys = []
+clear = False
 
-global l
-l = 0
-global b
-b = 0
-global s
-s = 0
-
-global clear
-clear = 0
-
-global col_clear
-col_clear = 0
 
 async def line(df):
-  if len(columns) > 0 and col_rep > -1:  
-    plt.figure()
-    plt.xlabel(columns[col_rep]["col1"])
-    plt.ylabel(columns[col_rep]["col2"])  
-    plt.plot(df[columns[col_rep]["col1"]], df[columns[col_rep]["col2"]], color = "#36453B")        
-    plt.savefig(my_path + "line.png", bbox_inches = "tight")
-    plt.close()
-    global l
-    l = 1
-    return
+    if len(columns) > 0:
+        plt.figure()
+        plt.xlabel(columns[-1]["col1"])
+        plt.ylabel(columns[-1]["col2"])
+        plt.plot(df[columns[-1]["col1"]],
+                 df[columns[-1]["col2"]], color="#36453B")
+        plt.savefig(my_path + "line.png", bbox_inches="tight")
+        plt.close()
+        global l
+        l = True
+        return
+
 
 async def bar(df):
-  if len(columns) > 0 and col_rep > -1:  
-    plt.figure()
-    plt.xlabel(columns[col_rep]["col1"])
-    plt.ylabel(columns[col_rep]["col2"])
-    plt.bar(df[columns[col_rep]["col1"]], df[columns[col_rep]["col2"]], color = "#36453B")        
-    plt.savefig(my_path + "bar.png", bbox_inches = "tight")
-    plt.close()
-    global b
-    b = 1
-    return
+    if len(columns) > 0:
+        plt.figure()
+        plt.xlabel(columns[-1]["col1"])
+        plt.ylabel(columns[-1]["col2"])
+        plt.bar(df[columns[-1]["col1"]],
+                df[columns[-1]["col2"]], color="#36453B")
+        plt.savefig(my_path + "bar.png", bbox_inches="tight")
+        plt.close()
+        global b
+        b = True
+        return
+
 
 async def scatter(df):
-  if len(columns) > 0 and col_rep > -1:  
-    plt.figure()
-    plt.xlabel(columns[col_rep]["col1"])
-    plt.ylabel(columns[col_rep]["col2"])    
-    plt.scatter(df[columns[col_rep]["col1"]], df[columns[col_rep]["col2"]], color = "#36453B")        
-    plt.savefig(my_path + "scatter.png", bbox_inches = "tight")
-    plt.close()
-    global s
-    s = 1
-    return
+    if len(columns) > 0:
+        plt.figure()
+        plt.xlabel(columns[-1]["col1"])
+        plt.ylabel(columns[-1]["col2"])
+        plt.scatter(df[columns[-1]["col1"]],
+                    df[columns[-1]["col2"]], color="#36453B")
+        plt.savefig(my_path + "scatter.png", bbox_inches="tight")
+        plt.close()
+        global s
+        s = True
+        return
+
 
 @app.route('/', methods=["GET", "POST"])
-def index():  
-  if request.method == "POST":
-    global col_rep
-    col_rep += 1
-    columns.append(request.form)
-    return redirect('/data')
-     
-  return render_template("index.html")
-  
+def index():
+    if request.method == "POST":
+        columns.append(request.form)
+        return redirect('/data')
+
+    return render_template("index.html")
+
+
 @app.route('/data', methods=['GET', 'POST'])
 def data():
-  global col_clear
-  global clear
-  global data_
-  global data_string
-  global columns
-  if col_clear == 1:
-    columns = []
-    col_clear = 0
-  if clear == 1:
-    data_ = []
-    data_string = ""
-    clear = 0   
-  if request.method == "POST":
-    global data_rep
-    data_rep += 1
-    data_.append(request.form)
-    return render_template("data.html", columns=columns, col_rep = col_rep, data_=data_, data_rep=data_rep)
-  if data != []:
-    return render_template("data.html", columns=columns, col_rep = col_rep, data_=data_, data_rep=data_rep)
-  else:
-    return render_template("data.html", columns=columns, col_rep = col_rep)
+    global clear
+    global data_
+    global data_string
+    global columns
+    if clear:
+        data_ = []
+        data_string = ""
+        clear = False
+    if request.method == "POST":
+        data_.append(request.form)
+        return render_template("data.html", columns=columns, data_=data_)
+    if data != []:
+        return render_template("data.html", columns=columns, data_=data_)
+    else:
+        return render_template("data.html", columns=columns)
 
-#https://flask.palletsprojects.com/en/2.0.x/async-await/
+
 @app.route('/graph', methods=['GET', 'POST'])
 async def graph_():
-  if request.method == "POST":
-    global data_string
-    data_string = ""
-    graph.append(request.form)
-    global graph_rep
-    graph_rep += 1
-    for x in data_:
-      row = str(x["data1"]) + "," + str(x["data2"]) + "\n"
-      data_string += row
-    df = pd.read_csv(StringIO(data_string), names = [columns[col_rep]["col1"], columns[col_rep]["col2"]]) 
-    
-    global l
-    global b
-    global s
-    l = 0
-    b = 0
-    s = 0
+    if request.method == "POST":
+        if len(data_) > 0:
+            global data_string
+            data_string = ""
+            graph.append(request.form)
+            for x in data_:
+                row = str(x["data1"]) + "," + str(x["data2"]) + "\n"
+                data_string += row
+            df = pd.read_csv(StringIO(data_string), names=[
+                columns[-1]["col1"], columns[-1]["col2"]])
+            global l
+            global b
+            global s
+            l = False
+            b = False
+            s = False
 
-    for x in graph[-1]:
-     
-      if x[0] == "l":        
-        await line(df)
-      if x[0] == "b":
-        await bar(df)  
-      if x[0] == "s":
-        await scatter(df)
-    
-    return render_template("graph.html", graph_data=data_string, graph_rep=graph_rep, graph=graph, path = 0,
-    l = l, b = b, s = s )    
-    
-  
-  return render_template("graph.html")
+            for x in graph[-1]:
+                if x[0] == "l":
+                    await line(df)
+                if x[0] == "b":
+                    await bar(df)
+                if x[0] == "s":
+                    await scatter(df)
+        return render_template("graph.html", graph_data=data_string, graph=graph,
+                               l=l, b=b, s=s)
+
+    return render_template("graph.html")
+
 
 @app.route('/clear', methods=["POST"])
 def clear():
-  
-  if request.method == "POST":
-      global clear
-      clear = 1
- 
-  return redirect("/data")
+    if request.method == "POST":
+        global clear
+        clear = True
 
-@app.route('/clearcol', methods=["POST"])
-
-def clearcol():
-  
-  if request.method == "POST":
-      global col_clear
-      col_clear = 1
-  return  redirect("/") 
- 
-
+    return redirect("/data")
 
 
 @app.route('/datatograph', methods=["POST"])
 def datatograph():
-     return redirect("/graph")  
+    return redirect("/graph")
 
-@app.route('/restart', methods=["POST"])
-def restart():
+
+@app.route('/clearrow', methods=["POST"])
+def clearrow():
     if request.method == "POST":
-      global clear
-      clear = 1
-      global col_clear
-      col_clear = 1
-    return redirect("/")  
+        global data_
+        data_ = data_[0:-1]
+
+    return redirect("/data")
+
 
 app.run(host='0.0.0.0', port=8080)
